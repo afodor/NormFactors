6ece11ab10f008763b01929e2a80d6033f6efb36
Anthony Fodor <anthony.fodor@gmail.com>
2014-10-04 17:35:24 -0400

    working towards automation...

---
+++ src/sim/WriteNormalized.java
@@ -0,0 +1,42 @@
+package sim;
+
+import java.io.File;
+
+import parsers.OtuWrapper;
+import utils.ConfigReader;
+
+public class WriteNormalized
+{
+	public static void main(String[] args) throws Exception
+	{
+		writeOne("resampledcontinious_.txt", -1);
+		writeOne("resampledcontinious_sameDepth.txt", 4181);
+		writeOne("resampled_.txt", -1);
+		writeOne("resampled_sameDepth.txt", 4181);
+	}
+	
+	private static void writeOne(String rawFileName, int rarifcationDepth) throws Exception
+	{
+		System.out.println(rawFileName + " " + rarifcationDepth);
+		
+		OtuWrapper wrapper = new OtuWrapper(new File(
+				ConfigReader.getBigDataScalingFactorsDir() 
+				+ File.separator + "risk" 
+				+ File.separator + "dirk" 
+				+ File.separator + "resample" + File.separator + 
+						rawFileName	));
+		
+		
+		wrapper.writeNormalizedDataToFile(new File(ConfigReader.getBigDataScalingFactorsDir() 
+				+ File.separator + "risk" 
+				+ File.separator + "dirk" 
+				+ File.separator + "resample" + File.separator + 
+						rawFileName.replaceAll(".txt", "") + "relativeAbundance.txt"));
+		
+		wrapper.writeRarifiedSpreadhseet(new File(ConfigReader.getBigDataScalingFactorsDir() 
+				+ File.separator + "risk" 
+				+ File.separator + "dirk" 
+				+ File.separator + "resample" + File.separator + 
+				rawFileName.replaceAll(".txt", "") + "rarified.txt"),false,rarifcationDepth);
+	}
+}
