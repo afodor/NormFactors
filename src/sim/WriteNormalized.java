d8fd289d05a018d3f82b879745b62798e153b543
afodor <anthony.fodor@gmail.com>
2014-10-08 10:27:25 -0400

    towards counts vs. distances

--- src/sim/WriteNormalized.java
+++ src/sim/WriteNormalized.java
@@ -9,12 +9,13 @@ public class WriteNormalized
 {
 	public static void main(String[] args) throws Exception
 	{
-		writeOne( "rnaResampledNonSparse.txt", -1);
+		writeOne("resampled_.txt", -1);
+		//writeOne( "rnaResampledNonSparse.txt", -1);
 		
 		//writeOne("rnaResampled.txt",-1);
 		
 		/*
-		writeOne("resampled_.txt", -1);
+		
 		writeOne("CF_LONG.txt", -1);
 		writeOne("may2013_refOTU_Table-subsetTaxaAsColumnsstoolOnly.filtered.txt", -1);
 		
